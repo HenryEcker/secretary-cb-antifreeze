7e88dbacbfecc8b088db46ae8764d09e436fd5d4
HenryEcker <hde1997@gmail.com>
2022-01-05 08:06:57 -0500

    Added logging

--- run.py
+++ run.py
@@ -3,29 +3,38 @@ from datetime import datetime
 from time import sleep
 
 import chatexchange
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
 
 
 def main(email, password, host_id, room_id, tries=3):
-    print(repr(host_id), repr(room_id))
-    exit(0)
     client = chatexchange.client.Client(host_id)
+    logging.info(f'Attempting to join Room {room_id} on "{host_id}"')
 
     attempt = 0
     while attempt < tries:
         client.login(email=email, password=password)
         attempt += 1
         if client.logged_in:
+            logging.debug(f'Login attempt {attempt} failed.')
             break
         sleep(20)
 
     # Must be logged in at this point
     assert client.logged_in
 
+    logging.info('Logged in')
+
     room = client.get_room(room_id)
     try:
+        # Join room
+        room.join()
+        logging.info('Joined Room')
         msg = f'{datetime.utcnow()}: Writer’s Block'
         # Send message
         room.send_message(msg)
+        logging.info(f'Message written: ({msg})')
     finally:
         # logout
         client.logout()
